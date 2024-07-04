08546a850289e8c87f82f736fb2fc7372a4d365f
yanghyeyeon <yhy4058@gmail.com>
2024-07-04 16:16:51 +0900

    ioc container application

--- chap01-ioc-container-lecture-source/src/main/java/com/ohgiraffers/section01/xmlconfig/Application.java
+++ chap01-ioc-container-lecture-source/src/main/java/com/ohgiraffers/section01/xmlconfig/Application.java
@@ -14,8 +14,17 @@ public class Application {
 
 //        MemberDTO member = new MemberDTO();
 
+        // ID(이름)으로 값 꺼내오기
         MemberDTO member = (MemberDTO) context.getBean("member");
 
-        System.out.println(member);
+        // 타입으로 값 꺼내오기
+        /* 타입으로 꺼내올 때는, MemberDTO 타입으로 등록된 빈이 하나가 아닐 수 있기 때문에
+        * 모호성 에러가 발생 할 수 있다.*/
+        MemberDTO member2 = context.getBean(MemberDTO.class);
+
+        // 타입과 id(이름)으로 값 꺼내오기
+        MemberDTO member3 = context.getBean("member", MemberDTO.class);
+
+        System.out.println(member3);
     }
 }
