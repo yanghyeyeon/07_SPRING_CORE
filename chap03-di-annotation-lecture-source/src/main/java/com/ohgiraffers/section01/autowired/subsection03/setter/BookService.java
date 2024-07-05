ea1047412a9c80c3447b5e69f01d2641bcb98098
yanghyeyeon <yhy4058@gmail.com>
2024-07-05 13:19:15 +0900

    autowired subsection03 setter

---
+++ chap03-di-annotation-lecture-source/src/main/java/com/ohgiraffers/section01/autowired/subsection03/setter/BookService.java
@@ -0,0 +1,29 @@
+package com.ohgiraffers.section01.autowired.subsection03.setter;
+
+import com.ohgiraffers.section01.autowired.common.BookDAO;
+import com.ohgiraffers.section01.autowired.common.BookDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("bookServiceSetter")
+public class BookService {
+
+
+    private BookDAO bookDAO;
+
+    // BookDAO 타입의 Bean 객체를 setter에 자동으로 주입해준다.
+    @Autowired
+    public void setBookDAO(BookDAO bookDAO) {
+        this.bookDAO = bookDAO;
+    }
+
+    public List<BookDTO> selectAllBooks() {
+        return bookDAO.selectBookList();
+    }
+
+    public BookDTO searchBookBySequence(int sequence) {
+        return bookDAO.selectOneBook(sequence);
+    }
+}
